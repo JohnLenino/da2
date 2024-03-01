629311a3cec10682a417fa2a001cb524b195d6f9
JohnLenino <konstantinbykov3122008@gmail.com>
2024-03-01 16:49:59 +0700

    da2

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import make_response
 
-from data.news import News
 from data.users import User
 from data import db_session
+from flask_login import LoginManager
+from flask import Flask, request, login_manager, render_template, redirect
+from flask_login import LoginForm, login_user
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -11,58 +14,59 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/blogs.db")
     db_sess = db_session.create_session()
-    user = User()
-    user.surname = 'Scott'
-    user.name = 'Ridley'
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = 'research engineer'
-    user.address = 'module_1'
-    user.email = 'scott_chief@mars.org'
+    # user = User()
+    # user.surname = 'Scott'
+    # user.name = 'Ridley'
+    # user.age = 21
+    # user.position = 'captain'
+    # user.speciality = 'research engineer'
+    # user.address = 'module_1'
+    # user.email = 'scott_chief@mars.org'
+    # db_sess = db_session.create_session()
+    # db_sess.add(user)
+
+    # db_sess.commit()
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
+
+
+@login_manager.user_loader
+def load_user(user_id):
     db_sess = db_session.create_session()
-    db_sess.add(user)
+    return db_sess.query(User).get(user_id)
 
-    db_sess.commit()
 
-    user = User()
-    user.surname = 'aboba'
-    user.name = 'bebrovich'
-    user.age = 42
-    user.position = 'moron'
-    user.speciality = 'moron'
-    user.address = 'module_2'
-    user.email = 'moron@mars.org'
-    db_sess = db_session.create_session()
-    db_sess.add(user)
-
-    db_sess.commit()
-
-    user = User()
-    user.surname = 'e'
-    user.name = 'a'
-    user.age = 21
-    user.position = 'servant'
-    user.speciality = 'ggg'
-    user.address = 'module_4'
-    user.email = 'ggg@mars.org'
-    db_sess = db_session.create_session()
-    db_sess.add(user)
-
-    db_sess.commit()
-
-    user = User()
-    user.surname = 'ee'
-    user.name = 'aa'
-    user.age = 2121
-    user.position = 'servant2'
-    user.speciality = 'ggkg'
-    user.address = 'module_78'
-    user.email = 'ggfg@mars.org'
-    db_sess = db_session.create_session()
-    db_sess.add(user)
-
-    db_sess.commit()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
     main()
+    app.run(port=8080, host='127.0.0.1')
+    login_manager = LoginManager()
+    login_manager.init_app(app)
