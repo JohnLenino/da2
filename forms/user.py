0482f858ea51b0539f0ccf4fbf4ecbeb35676ebc
JohnLenino <konstantinbykov3122008@gmail.com>
2024-03-05 14:27:51 +0700

    da3

--- forms/user.py
+++ forms/user.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, BooleanField, SubmitField, EmailField
+from wtforms import PasswordField, BooleanField, SubmitField, EmailField, StringField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
+    name =StringField('Имя', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
